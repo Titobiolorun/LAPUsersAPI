85a2251a2a342b56c138f9f146dd725c6e586206
Titobiolorun Ogundipe <t.ogundipe@fintraksoftware.com>
2025-09-01 22:24:57 +0100

    Deployment

--- Program.cs
+++ Program.cs
@@ -12,7 +12,13 @@ builder.Services.AddSwaggerGen();
 
 // Configure EF Core (replace with your connection string)
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"), sqlOptions =>
+    {
+        sqlOptions.EnableRetryOnFailure(
+            maxRetryCount: 3,
+            maxRetryDelay: TimeSpan.FromSeconds(30),
+            errorNumbersToAdd: null);
+    }));
 
 builder.Services.AddCors(options =>
 {
